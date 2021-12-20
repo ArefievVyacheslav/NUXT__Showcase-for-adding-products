32232e9a9abc58615c012d3d94431a84565b8f6d
Arefyev Vyacheslav <arefievvyacheslavspb@gmail.com>
2021-12-20 22:12:42 +0300

    layout default

---
+++ components/Aside.vue
@@ -0,0 +1,25 @@
+<template>
+  <aside class="aside">
+    <form class="form">
+      <h4 class="title-input">Наименование товара</h4>
+      <input type="text" placeholder="Введите наименование товара" class="input">
+      <h4 class="title-input">Описание товара</h4>
+      <textarea placeholder="Введите описание товара" class="area"></textarea>
+      <h4 class="title-input">Ссылка на изображение товара</h4>
+      <input type="text" placeholder="Введите ссылку" class="input">
+      <h4 class="title-input">Цена товара</h4>
+      <input type="text" placeholder="Введите цену" class="input"><br>
+      <button type="submit" class="send">Добавить товар</button>
+    </form>
+  </aside>
+</template>
+
+<script>
+    export default {
+        name: "Aside"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Header />
+    <div class="wrapper">
+      <Aside />
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "default"
+    }
+</script>
+
+<style scoped lang="sass">
+.container
+  max-width: 1376px
+  height: 100%
+  margin: 0 auto
+  background-color: #FAF9F7
+
+.wrapper
+  display: flex
+
+</style>
